5edf6a802605bff28f77973275260474bab8be08
Mostafa <mosi3883@gmail.com>
2022-05-11 00:18:12 +0430

    structure

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,54 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container show-container'>
+          <ul className='links'>
+            <li>
+              <a href='#1'>home</a>
+            </li>
+            <li>
+              <a href='#1'>about</a>
+            </li>
+            <li>
+              <a href='#1'>contact</a>
+            </li>
+            <li>
+              <a href='#1'>products</a>
+            </li>
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          <li>
+            <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
